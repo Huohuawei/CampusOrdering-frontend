51b74c4ccce21a40f0eb3be2d3afd93ab18d6365
Young9955 <112194219+Young9955@users.noreply.github.com>
2025-06-03 17:42:03 +0800

    Delete src directory

--- src/api/dish.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-// 获取指定商户的所有菜品
-export const getDishesByMerchantId = async (merchantId: number) => {
-  const response = await axios.get(`/api/dishes/merchant/${merchantId}`)
-  return response.data
-}
